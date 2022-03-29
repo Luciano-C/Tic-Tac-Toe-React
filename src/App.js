378c308e26d84dbf5aa618108afd157f15d652ce
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-29 15:48:51 -0300

    Funciona clickear y añadir los símbolos, falta lógica de quien gana

--- src/App.js
+++ src/App.js
@@ -11,95 +11,48 @@ import { Patterns } from "./Patterns"
 
 function App() {
 
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState("O");
-  const [result, setResult] = useState({winner: "none", state: "none"});
+  const [board, setBoard] = useState(["","","","","","","","",""]);
+  const [player, setPlayer] = useState("X");
 
-  
-  useEffect(() => {
-    checkIfTie();
-    checkWin();
+
+  const chooseSquare = (square) => {
+    // Recorre todo el tablero y si el cuadrado seleccionado es igual al index, reemplaza el valor por el valor de "player", sino lo deja como está
+    setBoard(board.map((value, index) => {
+      if (square === index && value === "") {
+        return player;
+      }
+      else {
+        return value;
+      }
+    }));
+    // Cambia el jugador en cada turno
     if (player === "X") {
       setPlayer("O");
     }
     else {
       setPlayer("X");
     }
-  }, [board])
-  
-
-  useEffect(() => {
-    if (result.state !== "none") {
-      alert(`Game finished! Winner is ${result.winner}`);
-      restartGame();
-    }
-    
-  }, [result])
-  
-  const chooseSquare = (squareToChange) => {
-    setBoard(board.map((val, index) => {
-      if (index === squareToChange && val === "") {
-        return player;
-      }
-      return val;
-    })
-    );
-
-
-
   };
 
-  const checkWin = () => {
-    Patterns.forEach((pattern) => {
-      const firstPlayer = board[pattern[0]];
-      if (firstPlayer === "") return;
-      let foundWinningPattern = true;
-      pattern.forEach((index) => {
-        if (board[index] !== firstPlayer) {
-          foundWinningPattern = false;
-        }
-      })
-      if (foundWinningPattern){
-        setResult({winner: player, state: "Won"})
-      }
-    });
-  };
-
-    const checkIfTie = () => {
-      let filled = true;
-      board.forEach((square) => {
-        if (square === "") {
-          filled = false;
-        }
-      }) 
-
-      if (filled) {
-        setResult({winner: "No one", state: "Tie"})
-      };
-    };
-
-    const restartGame = () => {
-      setBoard(["", "", "", "", "", "", "", "", ""]);
-      setPlayer("O");
-    }
+  
 
   return (
     <div className='App'>
       <div className='board'>
         <div className='row'>
-          <Square value={board[0]} chooseSquare={() => { chooseSquare(0) }} />
-          <Square value={board[1]} chooseSquare={() => { chooseSquare(1) }} />
-          <Square value={board[2]} chooseSquare={() => { chooseSquare(2) }} />
+          <Square value={board[0]} chooseSquare={() => { chooseSquare(0) }} id="s0"/>
+          <Square value={board[1]} chooseSquare={() => { chooseSquare(1) }} id="s1"/>
+          <Square value={board[2]} chooseSquare={() => { chooseSquare(2) }} id="s2"/>
         </div>
         <div className='row'>
-          <Square value={board[3]} chooseSquare={() => { chooseSquare(3) }} />
-          <Square value={board[4]} chooseSquare={() => { chooseSquare(4) }} />
-          <Square value={board[5]} chooseSquare={() => { chooseSquare(5) }} />
+          <Square value={board[3]} chooseSquare={() => { chooseSquare(3) }} id="s3"/>
+          <Square value={board[4]} chooseSquare={() => { chooseSquare(4) }} id="s4"/>
+          <Square value={board[5]} chooseSquare={() => { chooseSquare(5) }} id="s5"/>
         </div>
         <div className='row'>
-          <Square value={board[6]} chooseSquare={() => { chooseSquare(6) }} />
-          <Square value={board[7]} chooseSquare={() => { chooseSquare(7) }} />
-          <Square value={board[8]} chooseSquare={() => { chooseSquare(8) }} />
+          <Square value={board[6]} chooseSquare={() => { chooseSquare(6) }} id="s6"/>
+          <Square value={board[7]} chooseSquare={() => { chooseSquare(7) }} id="s7"/>
+          <Square value={board[8]} chooseSquare={() => { chooseSquare(8) }} id="s8"/>
         </div>
       </div>
     </div>
