657b3fd1f672ba9e18a49ce6e20c456feee63ae1
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-29 18:20:49 -0300

    Juego funcionando con p indicando turnos, triunfos o empates. Se añade además botón de reset. Pendiente elección inicial de X o O

--- src/App.js
+++ src/App.js
@@ -11,22 +11,28 @@ import { checkIfWin, checkIfTie } from "./checkIf"
 
 function App() {
 
-  const [board, setBoard] = useState(["","","","","","","","",""]);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("X");
   const [isGameOn, setIsGameOn] = useState(true);
+  const [paragraphText, setParagraphText] = useState("");
 
 
 
   useEffect(() => {
-   if (checkIfWin(board) !== "" && isGameOn) {
-     alert(`Winner is ${checkIfWin(board)}`)
-     setIsGameOn(false)
-   };
-   if (checkIfTie(board, isGameOn)) {
-     alert("Draw!")
-     setIsGameOn(false)
-   }
-  }, [board, isGameOn])
+    if (checkIfWin(board) !== "" && isGameOn) {
+      setIsGameOn(false)
+      setParagraphText(`The winner is ${checkIfWin(board)}!`)
+    }
+
+    else if (checkIfTie(board, isGameOn)) {
+      setIsGameOn(false)
+      setParagraphText("Draw!")
+    }
+    else if (isGameOn) {
+      setParagraphText(`It's ${player}'s turn`)
+    }
+    
+  }, [board, isGameOn, player])
 
   const chooseSquare = (square) => {
     // Recorre todo el tablero y si el cuadrado seleccionado es igual al index, reemplaza el valor por el valor de "player", sino lo deja como está
@@ -38,34 +44,43 @@ function App() {
         return value;
       }
     }));
-    // Cambia el jugador en cada turno
-    if (player === "X") {
+    
+    // Cambia jugador cada turno
+    if (player === "X" && isGameOn) {
       setPlayer("O");
     }
-    else {
+    else if (player === "O" && isGameOn) {
       setPlayer("X");
     }
+    
   };
 
-  
+  const startOver = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("X");
+    setIsGameOn(true);
+  }
+
 
   return (
     <div className='App'>
+      <p>{paragraphText}</p>
+      <button onClick={startOver} class="btn-grad">Start over</button>
       <div className='board'>
         <div className='row'>
-          <Square value={board[0]} chooseSquare={() => { chooseSquare(0) }} id="s0"/>
-          <Square value={board[1]} chooseSquare={() => { chooseSquare(1) }} id="s1"/>
-          <Square value={board[2]} chooseSquare={() => { chooseSquare(2) }} id="s2"/>
+          <Square value={board[0]} chooseSquare={() => { chooseSquare(0) }} id="s0" />
+          <Square value={board[1]} chooseSquare={() => { chooseSquare(1) }} id="s1" />
+          <Square value={board[2]} chooseSquare={() => { chooseSquare(2) }} id="s2" />
         </div>
         <div className='row'>
-          <Square value={board[3]} chooseSquare={() => { chooseSquare(3) }} id="s3"/>
-          <Square value={board[4]} chooseSquare={() => { chooseSquare(4) }} id="s4"/>
-          <Square value={board[5]} chooseSquare={() => { chooseSquare(5) }} id="s5"/>
+          <Square value={board[3]} chooseSquare={() => { chooseSquare(3) }} id="s3" />
+          <Square value={board[4]} chooseSquare={() => { chooseSquare(4) }} id="s4" />
+          <Square value={board[5]} chooseSquare={() => { chooseSquare(5) }} id="s5" />
         </div>
         <div className='row'>
-          <Square value={board[6]} chooseSquare={() => { chooseSquare(6) }} id="s6"/>
-          <Square value={board[7]} chooseSquare={() => { chooseSquare(7) }} id="s7"/>
-          <Square value={board[8]} chooseSquare={() => { chooseSquare(8) }} id="s8"/>
+          <Square value={board[6]} chooseSquare={() => { chooseSquare(6) }} id="s6" />
+          <Square value={board[7]} chooseSquare={() => { chooseSquare(7) }} id="s7" />
+          <Square value={board[8]} chooseSquare={() => { chooseSquare(8) }} id="s8" />
         </div>
       </div>
     </div>
